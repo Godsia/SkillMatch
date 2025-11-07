d9adc9ca72c642886916da64dc3c64c735b6164a
Godsia <151738440+Godsia@users.noreply.github.com>
2025-11-07 17:13:43 +0300

    initial

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Salam</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#74992e",
     alignItems: 'center',
     justifyContent: 'center',
   },
